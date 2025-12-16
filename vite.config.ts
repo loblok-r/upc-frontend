18076a5355b9317b9201cac88c34b7e9e989665d
柔丽君 <Chanloblok@163.com>
2025-12-16 13:39:44 +0800

    + 测试github page
    + 测试github page

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 添加这一行，/<仓库名>/
+  // 例如你的仓库叫 upc-frontend，这里就填 /upc-frontend/
+  base: '/upc-frontend/', 
 })
